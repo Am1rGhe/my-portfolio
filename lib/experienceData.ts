59553e9d61906323afa2342eb9808b9ff6f34b6f
Amir <amiramir2582004@.com>
2026-02-07 12:22:05 -0500

    add exeperience data

---
+++ lib/experienceData.ts
@@ -0,0 +1,17 @@
+export interface Experience {
+  year: string;
+  title: string;
+  description: string;
+  date?: string;
+}
+
+export const experiences: Experience[] = [
+  
+  { year: "Coming Soon", title: "Conference Volunteer ConFoo Montréal", description: "Volunteered at ConFoo, a major software development conference, assisting with event setup, technical checks, and on-site coordination to ensure sessions and equipment ran smoothly for speakers and attendees.", date: "Present" },
+  { year: "Currently", title: "Full-Stack Developer Fashion E-commerce Startup", description: "Currently leading the ongoing development and maintenance of the startup’s e-commerce platform, working across the frontend, backend logic, and database integration. Responsible for continuously improving features, performance, and overall user experience to support online sales and brand growth.", date: "Present" },
+  { year: "23 December 2025", title: "Database Tutor SQL & Relational Databases", description: "Led a complete end-to-end revision program covering SQL and relational database concepts (from A to Z) using Oracle Database, helping students prepare for exams through structured explanations, practice exercises, and project-oriented examples.", date: "Present" },
+  { year: "September 2025", title: "Programming Tutor Web & Java Fundamentals", description: "Delivered database courses covering SQL and relational database concepts using Oracle Database, while supporting students in understanding data modeling, queries, and database-driven projects.", date: "Present" },
+  { year: "September 2025", title: "Hackathon Organizer New Students (UQAM)", description: "Assisted in organizing a hackathon for new computer science students, contributing to event coordination, participant support, and creating a collaborative learning environment.", date: "2024" },
+  { year: "March 2025", title: "Founder & Project Lead Codyssey (Micro Club IT)", description: "Founded Codyssey, an educational initiative within Micro Club (the largest IT club in Algeria), creating a dedicated branch where students produce content and explain technical courses to support peer learning. Worked closely with the Head of the IT Department to structure the project, coordinate contributors, and guide its growth. \n Currently leading the development of a dedicated web platform, with an expected public release in summer 2026.", date: "2023" },
+  { year: "November 2023", title: "Ideathon Winner with my team", description: "Built a winning app idea enabling communities to locate and assist disaster affected individuals with essential aid.", date: "2023" },
+];
