de4238f0ce7b756f2b528191ab64a1ced902870d
Amir <amiramir2582004@.com>
2026-02-07 12:50:22 -0500

    add skills section in the website

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import Background from "@/components/Background";
 import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
+import SkillsSection from "@/components/sections/SkillsSection";
 import ExperienceSection from "@/components/sections/ExperienceSection";
 import ProjectsSection from "@/components/sections/ProjectsSection";
 import ContactSection from "@/components/sections/ContactSection";
@@ -16,6 +17,7 @@ export default function Home() {
         <Navbar />
         <HeroSection />
         <AboutSection />
+        <SkillsSection />
         <ExperienceSection />
         <ProjectsSection />
         <ContactSection />
